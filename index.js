3fe6438943bf56b439044e6eb8331a870dc494ad
mendosaurus <masaki.endo@nex.nikkei.com>
2019-11-02 15:10:28 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+function BlackjackGame() {
+    this.play = function () {
+        /*...*/ }; // Player wants to try their luck
+    this.stand = function () {
+        /*...*/ }; // Player wants to stand
+}
